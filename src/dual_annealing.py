c857922a37aa24a7aa895a9d2a16aa19811d4c3a
Edison Florez <edisonffh@gmail.com>
2021-09-03 19:44:34 +1200

    fisrt test and function to find minima for a function. Also, we update YML and dircetories layout

---
+++ src/dual_annealing.py
@@ -0,0 +1,67 @@
+import types
+from scipy.optimize import dual_annealing
+
+
+def solve_dual_annealing(function, bounds):
+    """Find the global minimum of a function using Dual Annealing [1].
+
+        Example:
+            lambda x: x ** 2
+            lambda x: (x[0] ** 2 + x[1] - 11) ** 2
+
+    Parameters
+    ----------
+    function : callable
+        The objective function to be minimized. Must be in the form f(x, *args),
+        where x is the argument in the form of a 1-D array and args is a tuple
+        of any additional fixed parameters needed to completely specify the function
+
+    bounds : sequence, shape (n, 2)
+        Bounds for variables. (min, max) pairs for each element in x, defining bounds
+        for the objective function parameter.
+
+    Returns
+    -------
+    Optimize Result
+        The optimization result represented as a OptimizeResult object. Important
+        attributes are: x the solution array, fun the value of the function at the
+        solution, and message which describes the cause of the termination.
+        See OptimizeResult for a description of other attributes.
+
+    Exception
+    ---------
+
+
+    Note
+    ----
+        References [1]: Tsallis C. Possible generalization of Boltzmann-Gibbs
+        statistics. Journal of Statistical Physics, 52, 479-487 (1998).
+
+        Check `scipy.optimize.dual_annealing` official documentation
+    """
+
+    if not bounds:
+        raise Exception(f"\n\n *** ERROR: No 'bounds' founds\n")
+
+    if not isinstance(function, types.FunctionType):
+        raise Exception(f"\n\n *** ERROR: No 'function' found\n")
+
+    result = dual_annealing(
+        function,
+        bounds=bounds,
+        maxiter=1000,
+        initial_temp=2000,
+    )
+
+    return result.x
+
+
+# # himmelblau = lambda x: (x[0] ** 2 + x[1] - 11) ** 2 + (x[0] + x[1] ** 2 - 7) ** 2
+# bounds = [(-5, 5), (-5, 5)]
+
+# solve_dual_annealing("himmelblau", bounds)
+
+# # first_annealing = himmelblau(solve_dual_annealing(himmelblau, bounds))
+
+# # print(isinstance(himmelblau, types.FunctionType))
+# # print(first_annealing)
---
+++ tests/test_dual_annealing.py
@@ -0,0 +1,40 @@
+import pytest
+import numpy as np
+from context import src
+from src.dual_annealing import solve_dual_annealing
+
+
+@pytest.mark.parametrize(
+    "cost_function, bounds, expected_minima",
+    [
+        (
+            lambda x: (x + 1) ** 2,
+            [(-10, 10)],
+            [0],
+        ),
+        (
+            lambda x: x ** 2,
+            [(-10, 10)],
+            [0],
+        ),
+        (
+            lambda x: (x[0] ** 2 + x[1] - 11) ** 2 + (x[0] + x[1] ** 2 - 7) ** 2,
+            [(-5, 5), (-5, 5)],
+            [0],
+        ),
+    ],
+)
+def test_solve_dual_annealing(cost_function, bounds, expected_minima):
+    """
+    Test for a function to find root using Dual Annealing procedure
+    """
+
+    points = solve_dual_annealing(cost_function, bounds)
+
+    global_minima = cost_function(points)
+
+    assert ((global_minima - expected_minima) < 1.0e-6).all()
+
+
+if __name__ == "__main__":
+    pytest.main(["tests"])
