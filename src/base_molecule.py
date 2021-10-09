f0d9836d746b0379a18e9e583f79146bd096f72a
e-florez <edisonffh@gmail.com>
2021-10-09 17:57:17 +1300

    Molecule almost done, starting Cluster class

--- src/base_molecule.py
+++ src/base_molecule.py
@@ -1,5 +1,4 @@
 from copy import deepcopy
-from typing import Type
 
 import attr
 import numpy as np
@@ -77,7 +76,7 @@ class Atom:
 
 
 # -------------------------------------------------------------
-@attr.s(frozen=True)
+@attr.s(frozen=False)
 class Molecule:
     """
     Create a Molecule that is at least ONE atom.
@@ -97,14 +96,14 @@ class Molecule:
         larger than zero, by defaul one (1)
     """
 
-    atoms = attr.ib()
+    _atoms = attr.ib()
     charge: int = attr.ib(default=0)
     multiplicity: int = attr.ib(default=1)
 
     # ===============================================================
     # VALIDATORS
     # ===============================================================
-    @atoms.validator
+    @_atoms.validator
     def _cehck_valid_atoms(self, attribute, atoms):
         for line, atom in enumerate(atoms):
             try:
@@ -148,7 +147,6 @@ class Molecule:
         atoms = atoms_dict.get("atoms")
         charge = atoms_dict.get("charge", 0)
         multiplicity = atoms_dict.get("multiplicity", 1)
-        # atoms = [Atom(*atom) for atom in atoms]
         return cls(atoms, charge, multiplicity)
 
     # ===============================================================
@@ -157,5 +155,194 @@ class Molecule:
     def __str__(self):
         return str(attr.asdict(self))
 
+    # ===============================================================
+    # PROPERTIES
+    # ===============================================================
+    @property
+    def atoms(self) -> list:
+        return self._atoms
+
+    @property
+    def atomic_masses(self) -> list:
+        return [Atom(*atom).atomic_mass for atom in self.atoms]
+
+    @property
+    def coordinates(self) -> list:
+        return [c[1:] for c in self.atoms]
+
+    @property
+    def elements(self) -> list:
+        """Show a list of unique symbols
+
+        Returns
+        -------
+        list
+            list of unique symbols
+        """
+        return list(set(self.symbols))
+
+    @property
+    def numbering_atoms(self) -> str:
+        """show atom number line by line
+
+        Returns
+        -------
+        str
+            atom number line by line
+        """
+        numbered_atoms = list()
+        for i in range(self.total_atoms):
+            line = list(self.atoms[i])
+            line[0] = f"\r  atom #{i} --> {line[0]:<6}"
+            line[1] = f"{line[1]:> 15.8f}"
+            line[2] = f"{line[2]:> 15.8f}"
+            line[3] = f"{line[3]:> 15.8f}"
+            numbered_atoms.append("".join(line))
+
+        return "\n".join(numbered_atoms)
+
+    @property
+    def symbols(self) -> list:
+        return [str(s[0]).title() for s in self.atoms]
+
+    @property
+    def total_atoms(self) -> int:
+        return len(self.atoms)
+
+    @property
+    def total_mass(self) -> float:
+        return sum(self.atomic_masses)
+
+    @property
+    def center_of_mass(self) -> tuple:
+        """Center of mass for a N-body problem. `Jacobi coordinates`_
+
+        Notes
+        -----
+            total mass for dummy atoms (not in the Periodic Table) is equal
+            to ONE (1)
+
+        Returns
+        -------
+        tuple : (float, float, float)
+            List of N 3D tuples, where N is equal to the number of atoms
+
+        .. _Jacobi coordinates:
+            https://en.wikipedia.org/wiki/Jacobi_coordinates
+        """
+
+        total_mass = 1 if not self.total_mass else self.total_mass
+
+        return tuple(
+            np.dot(
+                np.asarray(self.atomic_masses),
+                np.asarray(self.coordinates),
+            )
+            / total_mass
+        )
+
+    @property
+    def principal_axes(self) -> list:
+        return [
+            tuple(c)
+            for c in (
+                np.asarray(self.coordinates) - np.asarray(self.center_of_mass)
+            )
+        ]
+
+    @property
+    def xyz(self) -> str:
+        """Printing Molecule coordinates using XYZ format"""
+        comments = (
+            f"-- charge= {self.charge:<-g} and "
+            f"multiplicity= {self.multiplicity:<g} --"
+        )
+        write_xyz = f"""\t{self.total_atoms}\n{comments:<s}\n"""
+        for atom in self.atoms:
+            write_xyz += f"""{atom[0]:<6}"""
+            write_xyz += f"""\t{atom[1]:> 15.8f}"""
+            write_xyz += f"""\t{atom[2]:> 15.8f}"""
+            write_xyz += f"""\t{atom[3]:> 15.8f}\n"""
+
+        return write_xyz
+
 
 # -------------------------------------------------------------
+class Cluster(Molecule):
+    """
+    Create a Cluster with molecules/atoms to move and rotate
+    The format of the INPUT coordinates is as follows (any):
+
+    1. Dictionary type: {"atoms": [(<element> <X> <Y> <Z>), ...]}
+    2. List type: [(<element> <X> <Y> <Z>), ...]
+    3. Molecule/Cluster type (Objects)
+
+    Parameters
+    ----------
+    args : List, Dict, Molecule, Cluster
+        coordinates of each molecule/atom comma separates (support +,-,*)
+    frozen_molecule : integer, optional
+        fixing molecule to NOT move or rotate, by default NEGATIVE
+        integer means all molecules can be moved freely
+    sphere_radius : float, optional
+        radius for the spherical boundary condition, by default None
+    sphere_center : tuple, optional
+        Center of the sphere, by default (0, 0, 0)
+
+    Raises
+    ------
+    TypeError
+        [description]
+    """
+
+    def __init__(
+        self,
+        *args,
+        frozen_molecule: list = [],
+        sphere_radius: float = None,
+        sphere_center: tuple = (0, 0, 0),
+    ):
+        self._cluster_dict = dict()
+        self._multiplicity = 1
+        self._charge = 0
+
+        # fixing molecule to NOT move or rotate
+        self._frozen_molecule = frozen_molecule
+
+        # boundary conditions
+        self._sphere_radius = sphere_radius
+        self._sphere_center = sphere_center
+
+
+# -------------------------------------------------------------
+# -------------------------------------------------------------
+# -------------------------------------------------------------
+def test_Molecule():
+
+    # mol = Molecule([("a", 0, 0, 0), ("b", 10, 10, 10)], -10, 5))
+    # ---
+    mol = Molecule(
+        [("Xe", 0, 0, 0), ("Na", 5, 5, 5), ("Na", 10, 10, 10)], -10, 5
+    )
+
+    print("\n")
+    print(mol)
+    print("-" * 40)
+    print("atoms: :\n", mol.atoms)
+    print("charge:", mol.charge)
+    print("multiplicity:", mol.multiplicity)
+
+    print("-" * 40)
+    print("\nmasses: ", mol.atomic_masses)
+    print("\ntotal mass: ", mol.total_mass)
+    print("\nsymbols: ", mol.symbols)
+    print("\ntotal atoms: ", mol.total_atoms)
+    print("\nelements: ", mol.elements)
+
+    print("-" * 40)
+    print("\nnumbering atoms (index):\n", mol.numbering_atoms)
+    print(mol.xyz)
+
+    print("-" * 40)
+    print("\ncenter of mass: ", mol.center_of_mass)
+    print("\npp axes: ", mol.principal_axes)
