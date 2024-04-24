a255e2748b5ef954d8192c1719b5c6d7a7661349
AndyDanian <danianescobarv@gmail.com>
2024-04-24 12:06:01 -0300

    eliminate all attr function in atom class, because is more simple use __init__ and configurate with the super(). Moreover, this strategy avoid varibles innecessaries

--- amcess/atom.py
+++ amcess/atom.py
@@ -1,90 +1,63 @@
-import attr
-
-
 from rdkit.Chem.rdchem import Atom as RDKAtom
 
 from .data.atomic_data import atomic_mass
 
 
-@attr.s(frozen=True)
 class Atom(RDKAtom):
-    """
-    This class inherits attributes of the Atom class from RDKit, 
-    for more information:
-        *) https://www.rdkit.org/docs/cppapi/classRDKit_1_1Atom.html
-        *) https://www.rdkit.org/docs/source/rdkit.Chem.rdchem.html
-    
-    The idea of this class is to achieve of standard of amcess and
-    add atom coordinates to Atom class from RDKit.       
-
-    !Class description:
-    Representation of an individual atoms (<element> <X> <Y> <Z>)
-
-    .. rubric:: Examples
-
-    >>> Atom(element='H')
-    {'element': 'H', 'x': 0., 'y': 0., 'z': 0.}
-    NOTE: The coordinate by default are (0.,0.,0.)
-
-    >>> Atom('F', 0, 0, 1.97)
-    {'element': 'F', 'x': 0., 'y': 0., 'z': 1.97}
-
-    .. rubric:: Returns
-
-    atom : object
-        object like dict {'element': str, 'x': float, 'y': float, 'z': float}
-
-    .. rubric:: Raises
-
-    ValueError
-        format MUST be (str, float, float, float) with NOT empty filed
-    """
-
-    element: str = attr.ib()
-    x: int = attr.ib()
-    y: int = attr.ib()
-    z: int = attr.ib()
-
     # ===============================================================
-    # VALIDATORS
+    # Building
     # ===============================================================
-    # ! super.__init__() already check element
-    # @element.validator
-    # def _check_valid_element(self, element, value):
-    #     """Element: Must be valid NOT empty alphanumeric character"""
-    #     if not value.isalnum():
-    #         raise ValueError(
-    #             "\n\nMust be valid NOT empty alphanumeric character"
-    #             f"\nyou get --> '{value}'\n"
-    #         )
+    def __init__(
+        self, element: str = "", x: float = 0.0, y: float = 0.0, z: float = 0.0
+    ):
+        """
+        This class inherits attributes of the Atom class from RDKit,
+        for more information:
+            *) https://www.rdkit.org/docs/cppapi/classRDKit_1_1Atom.html
+            *) https://www.rdkit.org/docs/source/rdkit.Chem.rdchem.html
 
-    @x.validator
-    @y.validator
-    @z.validator
-    def _check_valid_point(self, coordinate, value):
-        """Coordinate: Must be valid float"""
-        if not isinstance(value, (int, float)):
-            raise ValueError(
-                "\n\nMust be valid NOT empty float"
-                f"\nyou get --> '{value}' with type: '{type(value).__name__}'"
-            )
+        The idea of this class is to achieve of standard of amcess and
+        add atom coordinates to Atom class from RDKit.
+
+        !Class description:
+        Representation of an individual atoms (<element> <X> <Y> <Z>)
+
+        .. rubric:: Examples
+
+        >>> Atom(element='H')
+        {'element': 'H', 'x': 0., 'y': 0., 'z': 0.}
+        NOTE: The coordinate by default are (0.,0.,0.)
+
+        >>> Atom('F', 0, 0, 1.97)
+        {'element': 'F', 'x': 0., 'y': 0., 'z': 1.97}
+
+        .. rubric:: Returns
+
+        atom : object
+            object like dict {'element': str, 'x': float, 'y': float, 'z': float}
+
+        .. rubric:: Raises
+
+        ValueError
+            format MUST be (str, float, float, float) with NOT empty filed
+        """
+        # Build: Atom class from RDKit
+        super().__init__(element)
+
+        # Coordinate: Must be valid float or int
+        for value in [x, y, z]:
+            if not isinstance(value, (int, float)):
+                raise ValueError(
+                    "\n\nMust be valid NOT empty float"
+                    f"\nyou get --> '{value}' with type: '{type(value).__name__}'"
+                )
+        self.x = x
+        self.y = y
+        self.z = z
 
     # ===============================================================
     # MAGIC METHODS
     # ===============================================================
     def __str__(self):
         """Magic method '__str__' to print the object as a dictionary"""
-        return str(attr.asdict(self))
-    
-    # ===============================================================
-    # Building
-    # ===============================================================
-    def __attrs_pre_init__(self, element, x: float = 0.0, y: float = 0.0, z: float = 0.0):
-        """
-        __attrs_pre_init__ is automatically detected and run before attrs
-        starts initializing. If __attrs_pre_init__ takes more than the
-        self argument, the attrs-generated __init__ will call it with the
-        same arguments it received itself. This is useful if you need to
-        inject a call to super().__init__()
-        """
-        super().__init__(element)
+        return f"atom: {self.GetSymbol()}, x: {self.x}, y: {self.y}, z: {self.z}"
